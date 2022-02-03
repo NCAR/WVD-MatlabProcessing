b9278e636ce5e9d5aa1b2f5eb8a45c241cbe1b67
stillwer <stillwel@ucar.edu>
2022-02-03 15:50:50 +0100

    Trying to make bootstrapping optional. Not yet working but getting close. ~RS

--- TemperatureRetrieval/RetrievalTemperature.m
+++ TemperatureRetrieval/RetrievalTemperature.m
@@ -3,10 +3,14 @@
 % Modificication Info: Created December, 2020
 
 
-function [TUnsmoothed,TVarUnsmoothed,TVarSmoothed,VarOld,Dt,MaxChange] = RetrievalTemperature(Op,Options,Paths,Data,PythonFile)
+function [Temp,Variance,Dt,MaxChange] = RetrievalTemperature(Op,Options,Paths,Data)
 %
+% Inputs: Op:      Full options structure
+%         Options: Temperature processing specific options
+%         Paths:
+%         Data:
 %
-%
+% Outputs:
 %
 %
 %% Temperature Pre-Process
@@ -17,13 +21,12 @@ Paths.PCA.Spectra       = {'20GHzPCA';'RB20GHzPCA'};   % Spectra to load
 Paths.PCA.SpectraLabels = {'Absorption';'RayleighBr'}; % Name of spectra in code
 % Pulling out and loading needed data
 [Counts.Raw,Data1D]     = IdentifyNeededInfo(Data);
-[Data1D.Surface,Data2D] = LoadPythonData(PythonFile);
-% MPD = Data2D.MPD;
+[Data1D.Surface,Data2D] = LoadPythonData(Paths.PythonData);
 % Loading data needed for processing
-Constants      = DefineConstants;
-Spectra.PCA    = ReadPCASpectra(Paths);
+Const       = DefineConstants;
+Spectra.PCA = ReadPCASpectra(Paths);
 % Reading Needed Data (Python HSRL and Receiver Scan)
-Spectra.Optics = ReadSystemScanData(Spectra.PCA,Constants);                % Should load calibration scan data
+Spectra.Optics = ReadSystemScanData(Spectra.PCA,Const);                % Should load calibration scan data
 % Bin lidar data to desired analysis resolution
 [Counts.Binned,BinInfo] = PreProcessLidarData(Counts.Raw,Options);
 % Downsample and interpolate ancillary data to known MPD grid
@@ -31,65 +34,103 @@ Data1D = RecursivelyInterpolate1DStructure(Data1D,Options.TimeStamp,'linear');
 Data2D = RecursivelyInterpolate2DStructure(Data2D,Options.TimeStamp,Options.Range,'linear');
 
 %% Bootstrapping
-% Looping over bootstrap iterations
-for m=1:1:Options.BootIters
-    CWLogging(['Bootstrap iteration: ',num2str(m),'\n'],Op,'Main')
-    CWLogging('     Poisson Thinning & Background Subtracting\n',Op,'Sub')
-    Thinned = PoissonThinLidarData(Counts.Binned,BinInfo,Options);
-    % Loop over each set of thinned data
-    for n=1:1:2
-        CWLogging('     Temperature pre-process\n',Op,'Sub')
-        Counts.BGSub = Thinned.(['PoissThined',num2str(n)]);
-        % Define guess atmosphere (lapse rate is a random starting variable)
-        GuessLapse =  -1*(0.0065 + rand.*(0.0098-0.0065));
-        ConstProfile = (Options.Range').*(GuessLapse);
-        Data2D.NCIP.Temperature.Value = repmat(ConstProfile,1,size(Data2D.NCIP.Temperature.Value,2))+Data1D.Surface.Temperature.Value';
-        % Build needed spectra
-        Spectra.Rebuilt = BuildSpectra(Spectra.PCA,Data2D.NCIP.Temperature,Data2D.NCIP.Pressure,Data1D.Wavelength,Op);
-        % Perterbative retrieval
-        CWLogging('     Perterbative Retrieval\n',Op,'Sub')
-        [Alpha,PerterbOrders] = PerturbativeRetrieval(Constants,Counts,Data2D,Options,Spectra,Op);
-        % Convert absorption to temperature
-        CWLogging('     Converting to temperature\n',Op,'Sub')
-        [Temperature{n,1},Dt{m,n}] = ConvertAlpha2Temperature(Alpha,Constants,Data1D,Data2D,Options,Data1D.Surface,Spectra,Op,'Bootstrap');
-        % Smooth temperature
-        Smoothing = MakeSmoothingKernal(Options);
-        Temperature{n,1}.Smoothed    = WeightedSmooth(Temperature{n,1}.Value,Smoothing);
-    end
-
-    % Calculate temperature average and variance from both poisson thinned profiles
-    CWLogging('     Calculating variance\n',Op,'Sub')
-    % Looping over teh raw and smoothed data set
-    n = 1;
-    for el = {'Value','Smoothed'}
+if Options.Bootstrap
+    % Looping over bootstrap iterations
+    for m=1:1:Options.BootIters
+        CWLogging(['Bootstrap iteration: ',num2str(m),'\n'],Op,'Main')
+        CWLogging('     Poisson Thinning & Background Subtracting\n',Op,'Sub')
+        Thinned = PoissonThinLidarData(Counts.Binned,BinInfo,Options);
+        % Pre-allocating variance variables that will later be overwritten
         if m == 1
-            % Pre-allocating the arrays to hold current and total variance
-            VarSum{n,1} = zeros(length(Options.Range),length(Options.TimeStamp));
-            Var{n,1}    = zeros(length(Options.Range),length(Options.TimeStamp));
+            Var = []; VarSum = [];
         end
-        Element = el{1};
-        % Calculating the average temperature from this bootstrap iteration
-        AvgTemp{m,n} = (Temperature{1,1}.(Element) + Temperature{2,1}.(Element))./2;
-        % Saving the old variance
-        VarOld{m,n}  = Var{n,1};
-        % Calculating the variance from this bootstrap iteration
-        VarSum{n,1} = VarSum{n,1} + (Temperature{1,1}.(Element) - Temperature{2,1}.(Element)).^2;
-        % Updating the current guess at variance
-        if m >= 2
-            Var{n,1} = (1./(2.*(m-1))).*VarSum{n,1};
-        else
-            Var{n,1} = (1./2).*VarSum{n,1};
+        % Loop over each set of thinned data
+        for n=1:1:2
+            CWLogging('     Temperature pre-process\n',Op,'Sub')
+            Counts.BGSub = Thinned.(['PoissThined',num2str(n)]);
+            % Define guess atmosphere (lapse rate is a random starting variable)
+            GuessLapse =  -1*(0.0065 + rand.*(0.0098-0.0065));
+            ConstProfile = (Options.Range').*(GuessLapse);
+            Data2D.NCIP.Temperature.Value = repmat(ConstProfile,1,size(Data2D.NCIP.Temperature.Value,2))+Data1D.Surface.Temperature.Value';
+            % Actually doing the nuts and bolts to retrieve temperature
+            [~,~,T{n,1},Dt{m,n}] = CalculateTemperature(Const,Counts,Data1D,Data2D,Options,Spectra,Op,'Bootstrap');
         end
-        % Calculate max change in variance for each iteration
-        MaxChange(m,n) = max(max(abs(Var{n,1} - VarOld{m,n})));
-        % Updating the loop counter
-        n = n + 1;
+        % Calculating variance
+        [AvgTemp{m},Var,MaxChange(m,:)] = CalculateVariance(Op,m,T,Var,VarSum);
     end
+    % Parsing out data for returning
+    Temp     = AvgTemp(:,1);
+    Variance = Var{1,1};
+
+else
+    % Background subtracting photons
+    CWLogging('     Background Subtracting\n',Op,'Sub')
+    Counts = BackgroundSubtractLidarData(Counts,CData,BinInfo,Op);
+    % Actually doing the nuts and bolts to retrieve temperature
+    [~,~,Temperature,Dt] = CalculateTemperatureclea(Const,Counts,Data1D,Data2D,Options,Spectra,Op,'Standard');
+    Temp      = Temperature.Value;
+    Variance  = [];
+    MaxChange = [];
 end
 
-%% Parsing out data for returning
-TUnsmoothed    = AvgTemp(:,1);
-TVarUnsmoothed = Var{1,1};
-TVarSmoothed   = Var{2,1};
+end
 
+
+function [Alpha,POrders,T,Dt] = CalculateTemperature(Const,Counts,Data1D,Data2D,Options,Spectra,Op,Type)
+%
+% Inputs:
+%          Type:        'Bootstrap'
+%
+%
+% Outputs: Alpha
+%          POrders     Perterbative retrieval orders
+%          T           Temperature
+%          Dt
+%
+%% Constructing the needed spectra for processing
+Spectra.Rebuilt = BuildSpectra(Spectra.PCA,Data2D.NCIP.Temperature,Data2D.NCIP.Pressure,Data1D.Wavelength,Op);
+%% Run the Perterbative Retrieval
+CWLogging('     Perterbative Retrieval\n',Op,'Sub')
+[Alpha,POrders] = PerturbativeRetrieval(Const,Counts,Data2D,Options,Spectra,Op);
+%% Convert absorption to temperature
+CWLogging('     Converting to temperature\n',Op,'Sub')
+[T,Dt] = ConvertAlpha2Temperature(Alpha,Const,Data1D,Data2D,Options,Data1D.Surface,Spectra,Op,Type);
+%% Smooth temperature
+CWLogging('     Smoothing temperature retrieval\n',Op,'Sub')
+T.Smoothed = WeightedSmooth(T.Value,Options);
+end
+
+function [AvgTemp,Var,MaxChange] = CalculateVariance(Op, m, Temperature, Var, VarSum)
+%
+%
+%
+%
+%% Calculate temperature average and variance from poisson thinned profiles
+CWLogging('     Calculating variance\n',Op,'Sub')
+% Looping over the raw and smoothed data set
+n = 1;
+for el = {'Value','Smoothed'}
+    if m == 1
+        % Pre-allocating the arrays to hold current and total variance
+        VarSum{n,1} = zeros(length(Options.Range),length(Options.TimeStamp));
+        Var{n,1}    = zeros(length(Options.Range),length(Options.TimeStamp));
+    end
+    Element = el{1};
+    % Calculating the average temperature from this bootstrap iteration
+    AvgTemp{1,n} = (Temperature{1,1}.(Element) + Temperature{2,1}.(Element))./2;
+    % Saving the old variance
+    VarOld{1,n}  = Var{n,1};
+    % Calculating the variance from this bootstrap iteration
+    VarSum{n,1} = VarSum{n,1} + (Temperature{1,1}.(Element) - Temperature{2,1}.(Element)).^2;
+    % Updating the current guess at variance
+    if m >= 2
+        Var{n,1} = (1./(2.*(m-1))).*VarSum{n,1};
+    else
+        Var{n,1} = (1./2).*VarSum{n,1};
+    end
+    % Calculate max change in variance for each iteration
+    MaxChange(1,n) = max(max(abs(Var{n,1} - VarOld{m,n})));
+    % Updating the loop counter
+    n = n + 1;
+end
 end

0e224d69682b1f7663824a8be0e875c92f0d42a5
stillwer <stillwel@ucar.edu>
2022-02-18 13:07:16 +0100

    Adding try/catch to check if data is availible. ~RS

--- TemperatureRetrieval/IdentifyNeededInfo.m
+++ TemperatureRetrieval/IdentifyNeededInfo.m
@@ -1,7 +1,7 @@
 
 
 
-function [Raw,Data1D] = IdentifyNeededInfo(Data)
+function [Raw,Data1D,Availible] = IdentifyNeededInfo(Data)
 %
 %
 %
@@ -10,13 +10,17 @@ function [Raw,Data1D] = IdentifyNeededInfo(Data)
 As = {'O2Online';'O2Offline'};
 for m=1:1:length(As)
     % Loading count profiles
-    Raw.(As{m}).TimeStamp = Data.Lidar.Interp.([As{m},'Comb']).TimeStamp.*60.*60;
-    Raw.(As{m}).Range     = (0:size(Data.Lidar.Interp.([As{m},'Comb']).Data,2)-1)'...
-                            .*mean(Data.Lidar.Interp.([As{m},'Comb']).RangeResolution.*299792458*1e-9/2,'omitnan');
-    Raw.(As{m}).Value = Data.Lidar.Interp.([As{m},'Comb']).Data';
-   
-   % Loading wavelength info
-   Data1D.Wavelength.(As{m}).TimeStamp = Data.TimeSeries.Laser.(As{m}).TimeStamp.*60.*60;
-   Data1D.Wavelength.(As{m}).Value = Data.TimeSeries.Laser.(As{m}).WavelengthActual./1e9;
+    try
+        Raw.(As{m}).TimeStamp = Data.Lidar.Interp.([As{m},'Comb']).TimeStamp.*60.*60;
+        Raw.(As{m}).Range     = (0:size(Data.Lidar.Interp.([As{m},'Comb']).Data,2)-1)'...
+                                 .*mean(Data.Lidar.Interp.([As{m},'Comb']).RangeResolution.*299792458*1e-9/2,'omitnan');
+        Raw.(As{m}).Value = Data.Lidar.Interp.([As{m},'Comb']).Data';
+        % Loading wavelength info
+        Data1D.Wavelength.(As{m}).TimeStamp = Data.TimeSeries.Laser.(As{m}).TimeStamp.*60.*60;
+        Data1D.Wavelength.(As{m}).Value = Data.TimeSeries.Laser.(As{m}).WavelengthActual./1e9;
+    catch
+        Raw = []; Data1D = [];
+        Availible = false;
+    end
 end
 end

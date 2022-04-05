143c06e4879bd1f7cc31f3b33497b519797d7111
stillwer <stillwel@ucar.edu>
2022-04-05 11:48:32 +0200

    Fixing wavemeter dropout visualization for field catalog. ~RS

--- TemperatureRetrieval/IdentifyNeededInfo.m
+++ TemperatureRetrieval/IdentifyNeededInfo.m
@@ -18,12 +18,15 @@ for m=1:1:length(As)
         Raw.(As{m}).Value = Data.Lidar.Interp.([As{m},Chan{m}]).Data';
         % Loading wavelength info
         Data1D.Wavelength.(As{m}).TimeStamp = Data.TimeSeries.Laser.(As{m}).TimeStamp.*60.*60;
-        Data1D.Wavelength.(As{m}).Value = Data.TimeSeries.Laser.(As{m}).WavelengthActual./1e9;
+        Data1D.Wavelength.(As{m}).Value   = Data.TimeSeries.Laser.(As{m}).WavelengthActual./1e9;
+        % Interpolating wavelength to handle wavemeter dropouts
+        Data1D.Wavelength.(As{m}).Value = InterpolateWavelength(Data1D.Wavelength.(As{m}).TimeStamp,...
+                                                                Data1D.Wavelength.(As{m}).Value);
         % Loading MCS info
         Data1D.MCS.(As{m}) = Data.Lidar.Interp.([As{m},Chan{m}]);
         Data1D.MCS.(As{m}) = rmfield(Data1D.MCS.(As{m}),'Data');
         Data1D.MCS.(As{m}).TimeStamp = Data1D.MCS.(As{m}).TimeStamp.*60.*60;
-        % Laoding wavelength scan info
+        % Loading wavelength scan info
         Scan.(As{m}) = Cal.ScanData.([As{m},Chan{m}]);
     catch
         Raw = []; Data1D = []; Scan = [];
@@ -31,4 +34,16 @@ for m=1:1:length(As)
         break
     end
 end
+end
+
+function [WLReturn] = InterpolateWavelength(Time,WL)
+%
+%
+%
+%% Removing bad data (i.e. data equal to nan)
+Time2 = Time;
+Time2(isnan(WL)) = [];
+WL(isnan(WL)) = [];
+%% Performing interpolation of data and excluding all extrapolation
+WLReturn = interp1(Time2,WL,Time,'linear',nan);
 end

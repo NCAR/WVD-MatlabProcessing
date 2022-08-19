e77cb164ce2c8f5bd015537db4cde4c3749cf545
stillwer <stillwel@ucar.edu>
2022-08-19 09:05:15 +0200

    Cleaning up comments. No functional change. ~RS

--- Definitions/DefineOptions.m
+++ Definitions/DefineOptions.m
@@ -27,66 +27,58 @@ Op.UploadFig     = ProcessHK | ProcessRet;
 Op.SaveFigures   = ProcessHK | ProcessRet;
 Op.SaveQuickLoad = ProcessRet; 
 Op.System        = System;
-
 %% Defining default options
 Op.Default.Frequency = linspace(-10,10,250);   % Units are gigahertz
 Op.Default.RangeRes  = 250;                    % Units are nanosceconds
 Op.Default.Range     = 16e3;                   % Units are kilometers
 Op.TimeGrid1d        = ((30:60:86400)./3600)'; % Data every 60 seconds
 Op.TimeGridLidar     = ((0:60:86400)./3600)';  % Data every 60 seconds
-
-Def.BackgroundInd = 10;       % How many pre-integration bins to use to estimate background noise
-Def.MaxTime       = 24*60*60; % Max time to run retrievals to          [seconds]
-Def.BinRange      = 2*37.5;   % Desired data range resolution          [meters]
-Def.MinRange      = 0;        % Start of retrievals                    [meters]
-Def.BlankRange    = 450;      % Altitude below which data is blanked   [meters]
-
+% Default settings (used to simplify definitions with common settings)
+Def.BackgroundInd = 10;       % Pre-int bins to estimate background noise
+Def.MaxTime       = 24*60*60; % Max time to run retrievals to         [sec]
+Def.BinRange      = 2*37.5;   % Desired data range resolution         [m]
+Def.MinRange      = 0;        % Start of retrievals                   [m]
+Def.BlankRange    = 450;      % Altitude below which data is blanked  [m]
 %% Defining Water Vapor retrieval options
 Op.WV             = Def;
-Op.WV.BinTime     = 4*60;     % Desired data time resolution           [seconds]
-Op.WV.GradFilt    = 1000;     % Relative count rate gradient to filter [ ]
-Op.WV.SmoothRange = 150;      % Desired smoothing range res            [meters]
-Op.WV.SmoothTime  = 4*60;     % Desired smoothing time res             [seconds]
-Op.WV.MaxRange    = 6.5e3;    % Max range to run retrievals to         [meters]
-Op.WV.MinRange    = 150;      % Start of retrievals                    [meters]
+Op.WV.BinTime     = 4*60;     % Desired data time resolution          [sec]
+Op.WV.GradFilt    = 1000;     % Count rate gradient to filter         [ ]
+Op.WV.MaxRange    = 6.5e3;    % Max range to run retrievals to        [m]
+Op.WV.MinRange    = 150;      % Start of retrievals                   [m]
+Op.WV.SmoothRange = 150;      % Desired smoothing range res           [m]
+Op.WV.SmoothTime  = 4*60;     % Desired smoothing time res            [sec]
 Op.WV             = MakeArrays(Op.WV);
-
 %% Defining HSRL retrieval options
 Op.HSRL             = Def;
-Op.HSRL.BinTime     = 2*60;     % Desired data time resolution           [seconds]
-Op.HSRL.BlankRange  = 250;      % Altitude below which data is blanked   [meters]
-Op.HSRL.SmoothRange = 2*37.5;      % Desired smoothing range res            [meters]
-Op.HSRL.SmoothTime  = 2*60;     % Desired smoothing time res             [seconds]
-Op.HSRL.MaxRange    = 8e3;    % Max range to run retrievals to         [meters]
-Op.HSRL.MinRange    = 0;                % Start of retrievals          [meters]
+Op.HSRL.BinTime     = 2*60;   % Desired data time resolution          [sec]
+Op.HSRL.BlankRange  = 250;    % Blank data from 0 to this range       [m]
+Op.HSRL.MaxRange    = 8e3;    % Max range to run retrievals to        [m]
+Op.HSRL.MinRange    = 0;      % Start of retrievals                   [m]
+Op.HSRL.SmoothRange = 2*37.5; % Desired smoothing range res           [m]
+Op.HSRL.SmoothTime  = 2*60;   % Desired smoothing time res            [sec]
 Op.HSRL             = MakeArrays(Op.HSRL);
-
 %% Defining Temperature retrieval options
 Op.Temp             = Def;
-Op.Temp.Method      = 'LBL';    % Options: 'LBL' (line-by-line) or 'PCA'
-Op.Temp.BinRange    = 4*37.5;   % Desired data range resolution          [meters]
-Op.Temp.BinTime     = 5*60;     % Desired data time resolution           [seconds]
-Op.Temp.BlankBSC    = 5e-5;     % Backscatter coefficient above which data is blanked
-Op.Temp.Bootstrap   = false;
-Op.Temp.BootIters   = 20;       % Iterations to use when bootstraping
-Op.Temp.SmoothRange = 300;      % Desired smoothing range res            [meters]
-Op.Temp.SmoothTime  = 15*60;    % Desired smoothing time res             [seconds]
-Op.Temp.TempIter    = 50;       % Iterations for temperature conversion
-Op.Temp.MaxRange    = 6e3;      % Max range to run retrievals to         [meters]=
-Op.Temp.MinRange    = 150;                % Start of retrievals          [meters]
+Op.Temp.Bootstrap   = false;  % Boolean to turn bootstrapping on
+Op.Temp.BootIters   = 20;     % Iterations to use when bootstraping
+Op.Temp.TempIter    = 50;     % Iterations for temperature conversion
+Op.Temp.Method      = 'LBL';  % Options: 'LBL' (line-by-line) or 'PCA'
+Op.Temp.BinRange    = 4*37.5; % Desired data range resolution         [m]
+Op.Temp.BinTime     = 5*60;   % Desired data time resolution          [sec]
+Op.Temp.BlankBSC    = 5e-5;   % Blank data from 0 to this range       [m]
+Op.Temp.MaxRange    = 6e3;    % Max range to run retrievals to        [m]
+Op.Temp.MinRange    = 150;    % Start of retrievals                   [m]
+Op.Temp.SmoothRange = 300;    % Desired smoothing range res           [m]
+Op.Temp.SmoothTime  = 15*60;  % Desired smoothing time res            [sec]
 Op.Temp             = MakeArrays(Op.Temp);
-
 %% Defining plotting options
-Op.Plot.FontSize     = 18;
-
-Op.Plot.RB.MaxRange  = 12e3;
-Op.Plot.RB.MinRange  = 0;
-Op.Plot.RB.CAxis     = [1,6];
-
-Op.Plot.WV.MaxRange  = 6e3;
-Op.Plot.WV.MinRange  = 0;
-Op.Plot.WV.CAxis     = [0,10];
-
+Op.Plot.FontSize    = 18;     % Font size to be applied to all figures
+Op.Plot.RB.MaxRange = 12e3;   % Max range to plot for field catalog   [m]
+Op.Plot.RB.MinRange = 0;      % Min range to plot for field catalog   [m]
+Op.Plot.RB.CAxis    = [1,6];  % Colorbar range for relative backscatter
+Op.Plot.WV.MaxRange = 6e3;    % Max range to plot for field catalog   [m]
+Op.Plot.WV.MinRange = 0;      % Min range to plot for field catalog   [m]
+Op.Plot.WV.CAxis    = [0,10]; % Colorbar range for absolute humidity
 %% Defining data to read 
 Op.DataNames = {'Container';'Current';'Etalon';'HumiditySensor';'Laser';'MCS';
                 'Power';'QuantumComposer';'Thermocouple';'UPS';'WeatherStation'}; 

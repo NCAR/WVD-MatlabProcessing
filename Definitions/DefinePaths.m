60bcbb332964ec04ed8f25d2a5ea413343d6940d
Robert Stillwell <stillwel@tikal.eol.ucar.edu>
2022-11-09 07:02:53 -0700

    Updating linux paths to run on other linux platforms than just fog. ~RS

--- Definitions/DefinePaths.m
+++ Definitions/DefinePaths.m
@@ -18,8 +18,8 @@ if ismac
     ProcBase = '/Volumes/MPD_ProcessedData';
     Server   = false;
 elseif isunix
-    DataBase = '/export/fog1/rsfdata/MPD';
-    CalBase  = '/export/fog1/rsfdata/MPD/calibration/eol-lidar-calvals';
+    DataBase = '/scr/fog1/rsfdata/MPD';
+    CalBase  = '/scr/fog1/rsfdata/MPD/calibration/eol-lidar-calvals';
     ProcBase = DataBase;
     Server   = true;
 end

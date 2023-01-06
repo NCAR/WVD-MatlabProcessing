42c6677c0810e6edca6993290874b0afdd4a8540
Robert Stillwell <stillwel@tikal.eol.ucar.edu>
2023-01-06 14:30:13 -0700

    Adding low count rate mask to WV and removing Speckle filter function from private here. ~RS

--- WVRetrieval/RetrievalWV.m
+++ WVRetrieval/RetrievalWV.m
@@ -85,14 +85,20 @@ MaskGrad = GradientFilter(Rb, Data1D.MCS.WVOffline, BinInfo, Options);
 MaskGrad = MaskGrad(1:size(MaskErr,1),1:size(MaskErr,2));
 % Remove high count rate regions
 MaskCntR = CntRate(Counts.BGSub.WVOffline.Counts, Data1D.MCS.WVOffline, BinInfo) > 2e6;
+% Romove low count rate regions
+CntsPerShot = Counts.BGSub.WVOffline.Counts./Data1D.MCS.WVOffline.ProfilesPerHistogram';
+MaskCntRLow = CntsPerShot < 0.01;
 % Combining the masks
 WV.Mask = MaskNP | MaskErr | MaskGrad | MaskCntR;
+WV.Mask2 = MaskNP | MaskErr | MaskGrad | MaskCntR | MaskCntRLow;
 % Removing data with high amounts of bad data neighbors (speckle filtering)
 Mask2   = DensityFiltering(WV.Mask,5,0.5);
+Mask3   = DensityFiltering(WV.Mask2,5,0.5);
 WV.Mask = WV.Mask | Mask2;
+WV.Mask2 = WV.Mask2 | Mask3;
 %% Calculating smoothed profile for plotting
 PreMask = WV.Value;
-PreMask(WV.Mask == 1) = nan;
+PreMask(WV.Mask2 == 1) = nan;
 WV.Smoothed2  = SmoothOld2(PreMask,Options);
 % %% Plotting
 % PlotWVMask(WV, MaskNP, MaskErr1, MaskErr2, MaskGrad, MaskCntR, Mask2)
@@ -174,39 +180,6 @@ function [Shifted] = Shift(Data)
 %
 %%
 Shifted = circshift(Data,[-1,0]);
-end
-
-function [Return] = DensityFiltering(Array,Points,Allowable)
-%
-% Inputs: Array:     An array of data to be filtered by nan density. This
-%                    array will be filtered assuming each measurement is a 
-%                    column.
-%         Points:    The number of points used to determine what the nan
-%                    density about a particular point is.
-%         Allowable: The maximum allowable density of nans about the data
-%                    point of interest
-%
-% Outputs: Mask:     An array of the size of the input Array with a nan
-%                    where density of input nans is above the allowable
-%                    level
-%
-%% Finding the starting point
-ToSkip = Points./2;
-Start  = floor(ToSkip);
-End    = floor(ToSkip);
-%% Arrays
-Mask = zeros(size(Array,1)-Points+1,size(Array,2)-Points+1); % Pre-allocating data
-for n=1:1:Points
-    for m=1:1:Points
-        Mask = Mask + Array(m:end-Points+m,n:end-Points+n);
-    end
-end
-Mask = Mask./Points./Points;
-%% Removing points with an unallowably high density of nans
-Return = Mask > Allowable;
-Return = [false(Start,size(Mask,2)+Start+End); 
-         [false(size(Mask,1),Start),Return,false(size(Mask,1),End)];
-          false(End,size(Mask,2)+Start+End)];
 end
 
 function [Navg] = SmoothOld2(N,Options)
